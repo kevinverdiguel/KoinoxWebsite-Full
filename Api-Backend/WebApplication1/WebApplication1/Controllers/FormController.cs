badb9e9c042f386d01ab00072b76100696092587
Dell <Dell@DESKTOP-NOUQ59H>
2023-12-14 11:52:18 -0600

    First commit

---
+++ Api-Backend/WebApplication1/WebApplication1/Controllers/FormController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller")]
+    public class FormController : Controller
+    {
+        private IConfiguration Configuration;
+
+        public FormController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddForms([FromBody] Form form)
+        {
+            string host = this.Configuration.GetValue<string>("Smtp: server");
+            int port = this.Configuration.GetValue<int>("Smtp: Port");
+            string fromAddress = this.Configuration.GetValue<string>("Smtp: FromAddress");
+            string userName = this.Configuration.GetValue<string>("Smtp: Username");
+            string password = this.Configuration.GetValue<string>("Smtp: Password");
+
+            using (MailMessage mm = new MailMessage(fromAddress, "admin@koinox.com"))
+            {
+                mm.Subject = "Nuevo contacto";
+
+            }
+
+        }
+    }
+}
---
+++ Api-Backend/WebApplication1/WebApplication1/Models/Form.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class Form
+    {
+        public Guid id { get; set; }
+
+        public string name { get; set; }
+
+        public string correo { get; set; }
+
+        public string mensaje { get; set; }
+    }
+
+    public class EmailMessage
+    {
+        public List<Form> ToAddresses { get; set; } = new List<Form>();
+        public List<Form> FromAddresses { get; set; } = new List<Form>();
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+
+}
